27ddcbf5fb650a88c6d9d636ec9a0423906c898a
Mohnish Lokhande <mohnishlokhande14@gmail.com>
2021-04-13 13:30:00 +0530

    inSeparateFile

--- index.js
+++ index.js
@@ -1,61 +1,12 @@
 const express = require("express")
 const app =express();
 const PORT=6969;
-const userData =require("./MOCK_DATA.json");
-const graphql =require('graphql');
-const {GraphQLObjectType,GraphQLSchema, GraphQLInt, GraphQLString, GraphQLList} =require('graphql');
 const {graphqlHTTP} =require('express-graphql');
+const schema = require("./Schemas/index");
+const cors =require("cors");
 
-const UserType =new GraphQLObjectType({
-    name:"User",
-    fields:()=>({
-        id: {type:GraphQLInt},
-        firstName: {type:GraphQLString},
-        lastName: {type:GraphQLString},
-        email: {type:GraphQLString},
-        password: {type:GraphQLString},
-    })
-})
-
-const RootQuery = new GraphQLObjectType({
-    name: "RootQueryType",
-    fields:{
-        getAllUser: {
-            type: new GraphQLList(UserType),
-            args:{id:{type: GraphQLInt}},
-            resolve(parent, args){
-                return userData
-            }
-        }
-    }
-})
-const Mutation = new GraphQLObjectType({
-    name: "Mutation",
-    fields: {
-        createUser:{
-            type: UserType,
-            args:{
-                firstName:{type:GraphQLString},
-                lastName:{type:GraphQLString},
-                email:{type:GraphQLString},
-                password:{type:GraphQLString},
-            },
-            resolve(parent, args){
-                userData.push({
-                    id: userData.length+1,
-                    firstName: args.firstName,
-                    lastName: args.lastName,
-                    email: args.email,
-                    password: args.password
-                })
-                return args
-            }
-        }
-    }
-})
-
-const schema =new GraphQLSchema({query: RootQuery, mutation: Mutation})
-
+app.use(cors());
+app.use(express.json());
 app.use('/graphql',graphqlHTTP({
     schema,
     graphiql: true
